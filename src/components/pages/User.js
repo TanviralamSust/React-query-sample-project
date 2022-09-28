cba7e62565b995a965b74307e52f40f8df87baac
TanviralamSust <tanviralam997@gmail.com>
2022-09-28 15:16:30 -0600

    Integrate comment list in frontend

--- src/components/pages/User.js
+++ src/components/pages/User.js
@@ -1,3 +1,30 @@
+import { useMemo } from "react";
+import useUserData from "../../hooks/useUserData.js";
+import "../../styles/Home.scss";
+
 export default function User() {
-  return <div>User LIST indiviual</div>;
+  const { userData, error, loading } = useUserData(
+      "GET",
+      "https://jsonplaceholder.typicode.com/posts/1/comments",
+      {}
+  );
+  const cachedComment = useMemo(() => {
+    return userData || {};
+  }, [userData]);
+
+  return (
+      <div className="home-container">
+        {!loading && cachedComment.length === 0 && <div> No Data Found! </div>}
+        {error && <div> Error Found! </div>}
+        {loading && <div> Loading </div>}
+        {cachedComment.map((comment, index) => (
+            <div className="post-container" key={index}>
+              <span className="post-details">{comment.id}</span>
+              <span className="post-details">{comment.name}</span>
+              <span className="post-details">{comment.email}</span>
+              <span className="post-details">{comment.body}</span>
+            </div>
+        ))}
+      </div>
+  )
 }
